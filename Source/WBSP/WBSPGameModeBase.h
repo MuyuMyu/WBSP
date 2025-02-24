2f16ed3d5441c2a0a744440cd2404b34b38c7c14
MuyuMyu <muyu28372@gmail.com>
2025-02-24 14:44:14 +0800

    First

---
+++ Source/WBSP/WBSPGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WBSPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WBSP_API AWBSPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
